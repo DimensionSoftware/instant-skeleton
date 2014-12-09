485860078e0f1c3dd0324b3886851866459d1135
mr. keith <keith@hoerling.com>
2014-12-08 18:27:00 -0800

    removed node_modules and updated .gitignore
    - TODO fork all repos needing tweaks
    - hope this repo can be compacted; otherwise, it needs a re-import  ;/

--- node_modules/multilevel/lib/write-manifest.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var createManifest = require('level-manifest');
-
-module.exports = writeManifest;
-
-function writeManifest (db, path) {
-  var manifest = createManifest(db);
-  fs.writeFileSync(path, JSON.stringify(manifest));
-}
--- node_modules/multilevel/msgpack.js
+++
@@ -1,7 +0,0 @@
-var MuxDemux = require('mux-demux/msgpack');
-
-module.exports = {
-  client: require('./lib/client')(MuxDemux),
-  server: require('./lib/server')(MuxDemux),
-  writeManifest: require('./lib/write-manifest')
-};
