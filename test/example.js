9b9f4578fe0b2af966f8ca62e5afa1162e5d7d20
mr. keith <keith@hoerling.com>
2015-01-22 14:49:16 -0800

    fix: "npm test" working
    - adds test for booting the app (async)
    - would these be better in livescript?

--- test/example.js
+++ test/example.js
@@ -2,19 +2,28 @@ var expect  = require('chai').expect,
     sinon   = require('sinon'),
     request = require('supertest'),
     App     = require('../build/server/App.js'),
-    app     = new App()
+    app     = new App(process.env.npm_package_config_node_port-1)
+
+server = null
 
 
-server = app.start().server;
+// TODO better coverage
+describe('Can we boot the app?', function () {
+  this.timeout(5000);
+  before(function (done) {
+    app.start(function() {
+      server = app.server // set app instance
+      done()
+    })
+  });
 
-describe('Can we fetch an index page?', function () {
-  it('fetches', function (done) {
-    request(server)
-      .get('/')
-      .expect(200)
-      .end(function(err, data){
-        if (err) return done(err);
-        done();
-      })
+  describe('Can we fetch an index page?', function () {
+    it('fetches', function (done) {
+      request(server)
+        .get('/')
+        .expect(200, done)
+    });
   });
 });
+
+
