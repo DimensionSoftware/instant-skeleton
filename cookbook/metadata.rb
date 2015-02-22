6f7b5660202bba4ce0c99300c81c18ed6bc6d216
mr. keith <keith@hoerling.com>
2015-02-21 21:45:59 -0800

    prune cookbook (removing all deploy bits to shrinking the skeleton tiny as possible)
    - "git revert" if you need this commit
    
    
    Former-commit-id: 854229c08bb7459d2ea9f75df146cab9e751be0c [formerly 37a33b431be18a9377278b4583c11ea8ecf96778]
    Former-commit-id: bb11cd70c6c0fcd015748a2b55c593201de008b5

--- cookbook/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'instant_skeleton'
-maintainer       'Instant Channel'
-maintainer_email 'support@instantchannelinc.com'
-license          'All rights reserved'
-description      'Installs/Configures instant-skeleton'
-long_description 'Installs/Configures instant-skeleton'
-version          '0.1.0'
-
-depends 'nodejs'
