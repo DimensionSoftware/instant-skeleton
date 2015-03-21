64ffef11acdc565032a7d346c58093779c65827e
mr. keith <keith@hoerling.com>
2015-03-20 20:43:29 -0700

    fix: webpack works in production
    Former-commit-id: c2ee1bd2afc1eae76a12b9b78697c586260c7ad1
    Former-commit-id: f8c4113d80fd1dedcb0087206d93464e46dc425c

--- webpack.config.js
+++ webpack.config.js
@@ -9,19 +9,25 @@ var env       = process.env.NODE_ENV || 'development'
   , subdomain = process.env.npm_package_config_subdomain || 'develop.com'
   , dev_port  = process.env.npm_package_config_dev_port  || 8081;
 
-
 var entry =
   { client: ['./client/layout' ] }
 
 var plugins =
   [ new webpack.optimize.DedupePlugin()
-  , new ExtractText('site.css', {allChunks:true})
+  , new ExtractText('site.css', { allChunks:true })
   ]
-if (prod) // production plugins
+var loaders =
+  [ { test: /\.styl$/, loader: ExtractText.extract('css-loader!stylus-loader') } ]
+
+// init
+// ----
+if (prod) { // production settings
   plugins.push(new webpack.optimize.UglifyJsPlugin())
-else
+  loaders.push({ test: /\.ls$/, loader: 'livescript-loader?const=true' })
+} else {
   plugins.push(new webpack.HotModuleReplacementPlugin())
   // add hot-reload
+  loaders.push({ test: /\.ls$/, loaders: ['react-hot', 'livescript-loader?const=true'] })
   entry.client.push
     ('webpack/hot/dev-server'
     , 'webpack-dev-server/client?http://'
@@ -29,25 +35,23 @@ else
       + ':'
       + dev_port
     )
+}
 
-
+// main
+// ----
 module.exports =
   { cache: !prod
   , context:  __dirname
   , debug:    !prod
+  , quiet:    prod
   , devtool:  'source-map'
   , optimize: prod
   , entry:    entry
   , plugins:  plugins
   , resolve:  { extensions: ['', '.ls', '.js', '.styl'] }
-  , module:
-    { loaders:
-      [ { test: /\.ls$/,   loaders: ['react-hot', 'livescript-loader?const=true'] }
-      , { test: /\.styl$/, loader: ExtractText.extract('css-loader!stylus-loader') }
-      ]
-    }
-  , stylus: { use: [nib()] }
-  , node:   { fs: 'empty' }
+  , module:   { loaders: loaders }
+  , stylus:   { use: [nib()] }
+  , node:     { fs: 'empty' }
   , output:
     { path:       path.join(__dirname, 'public/builds')
     , filename:   "[name].js"
